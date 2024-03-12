d19ee595df23110b498bd301b85d8770df47759b
mihagan20 <spidermanps69@gmail.com>
2024-03-12 23:33:28 +0300

    Initial commit

---
+++ Source/Test_Game_1/Test_Game_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Test_Game_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test_Game_1, "Test_Game_1" );
---
+++ Source/Test_Game_1/Test_Game_1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Test_Game_1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_GAME_1_API ATest_Game_1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
